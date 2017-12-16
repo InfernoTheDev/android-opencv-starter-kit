bb67958aa6f71de63125fd185428ffa60d453350
no_parinya <parinya.k09@gmail.com>
2017-12-16 11:51:35 +0700

    init

---
+++ app/src/main/java/project/illnino/com/facerec/MainActivity.java
@@ -0,0 +1,30 @@
+package project.illnino.com.facerec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    static{
+        //System.loadLibrary("opencv_java");
+        System.loadLibrary("opencv_java3");
+
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+        } else {
+            Log.d(TAG, "OpenCV loaded");
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
